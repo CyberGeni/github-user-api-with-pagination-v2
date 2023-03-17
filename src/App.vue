f03c96aaa46607126cad2dfba1ee53a9b66c84c6
Treasure Ajefu <treasureajefu@gmail.com>
2023-03-17 19:23:38 +0100

    did a cmd themed ui, not sure how i feel about it

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
-<template>
-  <RepoList />
+<template class="">
+  <div class="bg-zinc-900 min-h-screen text-sky-200">
+    <header class="border-b border-sky-600 py-6">
+      <div class="font-['VT323'] text-3xl">cybergenie</div>
+    </header>
+    <main class="w-5/6 border-x border-sky-600 mx-auto h-full ">
+      <RepoList />
+    </main>
+    
+  </div>
 </template>
 
 <script>
-import RepoList from './components/RepoList.vue'
+import RepoList from "./components/RepoList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    RepoList
-  }
-}
+    RepoList,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/RepoList.vue
+++ src/components/RepoList.vue
@@ -1,34 +1,36 @@
 <template>
-  <div>
-    <h1>My Repositories</h1>
-    <ul>
-      <li v-for="repo in paginatedRepos" :key="repo.id">
-        <router-link :to="{ name: 'repo', params: { id: repo.name } }">{{ repo.name }}</router-link>
+  <div class="p-12">
+    <ul class="font-['Anonymous_Pro'] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
+      <li class="p-6 border flex items-center" v-for="repo in paginatedRepos" :key="repo.id">
+        <router-link :to="{ name: 'repo', params: { id: repo.name } }">
+          {{ repo.name }}
+        </router-link>
       </li>
     </ul>
-    <div>
-      <button v-if="currentPage > 1" @click="previousPage">Previous</button>
-      <span>{{ currentPage }}</span>
-      <button v-if="currentPage < totalPages" @click="nextPage">Next</button>
+    <div class="space-x-4 mt-12">
+      <button class="border px-6 py-2" v-if="currentPage > 1" @click="previousPage">Previous</button>
+      <span class="border px-6 py-2">{{ currentPage }}</span>
+      <span class="border px-6 py-2">{{ currentPage + 1 }}</span>
+      <button class="border px-6 py-2" v-if="currentPage < totalPages" @click="nextPage">Next</button>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import { routerLink } from 'vue-router';
+import axios from "axios";
+import { routerLink } from "vue-router";
 
 export default {
   components: {
-    routerLink
+    routerLink,
   },
   data() {
     return {
       repos: [],
-      perPage: 10,
+      perPage: 9,
       currentPage: 1,
       totalPages: 1,
-      username: 'cybergeni',
+      username: "cybergeni",
     };
   },
   mounted() {
@@ -37,7 +39,8 @@ export default {
   methods: {
     fetchRepos() {
       const apiUrl = `https://api.github.com/users/${this.username}/repos`;
-      axios.get(apiUrl)
+      axios
+        .get(apiUrl)
         .then((response) => {
           this.repos = response.data;
           this.totalPages = Math.ceil(this.repos.length / this.perPage);
